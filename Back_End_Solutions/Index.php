7344f9fde8964ff80c4afc4387ae9e9c8cbc5950
VioletaTaneva <violeta.akaunt11@gmail.com>
2025-02-19 10:35:38 +0100

    Index update

--- Back_End_Solutions/Index.php
+++ Back_End_Solutions/Index.php
@@ -1,48 +1,56 @@
-<?php
-    $fruit = 'coconut';
-    $fruit_letter_count = strlen($fruit);
-    $fruit_letter_find = strpos($fruit, 'o') + 1; // Add +1 cause the sting counts from 0 and it is technically incorect in my opinion
-
-    $fruit2 = 'pineapplea';
-    $fruit2_letter_count = strlen($fruit2);
-    $fruit2_letter_find = strrpos($fruit2, 'a') + 1; 
-    $fruit2_uppercase = strtoupper($fruit2);
-
-    $letter = 'e';
-    $number = '3';
-    $longerst_word = 'pneumonoultramicroscopicsilicovolcanoconiosis';
-    $longerst_word_replaced = str_replace($letter, $number, $longerst_word);
-?>
-
 <!DOCTYPE html>
-<html> 
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="/css/global.css">
-    <link rel="stylesheet" type="text/css" href="/css/directory.css">
-    <link rel="stylesheet" type="text/css" href="/css/facade.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercises</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            display:flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            background-color: #f4f4f4;
+        }
+
+        .navbar {
+            list-style-type: none;
+            padding: 0;
+            background-color: #333;
+            border-radius: 5px;
+            text-align: center;
+            width: 200px;
+        }
+
+        .navbar li {
+            margin: 10px 0px;
+        }
+
+        .navbar a {
+            text-decoration: none;
+            color: white;
+            font-size: 18px;
+            padding: 10px 20px;
+            display: block;
+            border-radius: 5px;
+        }
+
+        .navbar a:hover {
+            background-color: #575757;
+        }
+    </style>
 </head>
-
 <body>
-    <h1>Part 1</h1>
-    <h2><?php echo $fruit; ?></h2>
-    <p>Character Count: <?php echo $fruit_letter_count; ?></p>
-    <p>First 'o' Position: <?php echo $fruit_letter_find; ?></p>
 
-    <h1>Part 2</h1>
-    <h2><?php echo $fruit2; ?></h2>
-    <p><?php echo $fruit2_uppercase; ?></p>
-    <p>Character Count: <?php echo $fruit2_letter_count; ?></p>
-    <p>Last 'a' Position: <?php echo $fruit2_letter_find; ?></p>
-
-    <h1>Part 3</h1>
-    <h2>Original word: <?php echo $longerst_word; ?></h2>
-    <h2> Replaced letter "<?php echo$letter?>":  <?php echo $longerst_word_replaced;?> </h2>
-   
+    <ul class="navbar">
+        <li><a href="exercise1.php">Exercise 1</a></li>
+        <li><a href="exercise2.php">Exercise 2</a></li>
+        <li><a href="exercise3.php">Exercise 3</a></li>
+        <li><a href="exercise3.php">Exercise 4</a></li>
+    </ul>
 
 </body>
 </html>
-
-
-
